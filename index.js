79214251c3d1131d40928ab788c84993d91824a7
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-04-04 18:34:49 -0400

    Complete home page of the website

--- index.js
+++ index.js
@@ -2,13 +2,27 @@
 const express = require("express");
 const path = require("path");
 
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 // Setting the express
 const app = express();
 const port = process.env.PORT || "8888";
 
+// Setting the template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Setting up the folder for the static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // main page
 app.get("/", async (req, res) => {
-  res.send("Your app is running successfully");
+  res.render("home/index");
 });
 
 // Set up the server listening
